7444ad0ee5ed110ab68b2803a0c42be1299f368d
Sparsematrix09 <bt23cse041@iiitn.ac.in>
2026-02-06 19:21:22 +0530

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
 
-// next.config.ts
-const nextConfig = {
-  // Skip API routes during build
-  async redirects() {
-    return [
-      {cacheComponents: true,
-        source: '/api/:path*',
-        destination: '/',
-        permanent: false,
-        missing: [
-          {
-            type: 'header',
-            key: 'x-vercel-build',
-          },
-        ],
-      },
-    ];
-  },
+const nextConfig: NextConfig = {
+  cacheComponents: true,
 };
 
 export default nextConfig;
